d684c8cdcfd1f57e832e2e831cd49c73e27df7b8
Amantina Sosa <amantinasosa@gmail.com>
2020-09-10 07:18:14 -0400

    part one done

--- src/requests.js
+++ src/requests.js
@@ -12,18 +12,3 @@ const requests = {
 };
 
 export default requests;
-
-// process.env.APIKEY
-
-// const requests = {
-//   fetchTrending: `/trending/all/week?api_key=${process.env.APIKEY}&language=en-US`,
-//   fetchNetflixOriginals: `/discover/tv?api_key=${process.env.APIKEY}&with_networks=213`,
-//   fetchTopRated: `/movie/top_rated?api_key=${process.env.APIKEY}&language=en-US`,
-//   fetchActionMovies: `/discover/movie?api_key=${process.env.APIKEY}&with_genres=28`,
-//   fetchComedyMovies: `/discover/movie?api_key=${process.env.APIKEY}&with_genres=35`,
-//   fetchHorrroMovies: `/discover/movie?api_key=${process.env.APIKEY}&with_genres=27`,
-//   fetchRomanceMovies: `/discover/movie?api_key=${process.env.APIKEY}&with_genres=10749`,
-//   fetchDocumentaries: `/discover/movie?api_key=${process.env.APIKEY}&with_genres=99`,
-// };
-
-// export default requests;
